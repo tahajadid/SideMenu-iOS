37342d40d2bceed8de1a45b1947cdcf004204fa7
Taha Jadid <jadid.taha@waroom.com>
2023-02-23 23:23:20 +0100

    Add custom menu cells

--- TMovies/Controllers/SideMenu/SideMenuViewController.swift
+++ TMovies/Controllers/SideMenu/SideMenuViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SideMenuViewController: UIViewController {
     
+    private let reuseIdentifier = "SideMenuCell"
+    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var logoutView: UIView!
     @IBOutlet weak var menuOptionTableView: UITableView!
@@ -20,6 +22,25 @@ class SideMenuViewController: UIViewController {
         profileImage.layer.cornerRadius = profileImage.frame.height/2
         profileImage.clipsToBounds = true
         // Do any additional setup after loading the view.
+        
+        menuOptionTableView.dataSource = self
+        menuOptionTableView.delegate = self
+        
+        let nib = UINib(nibName: reuseIdentifier, bundle: nil)
+        menuOptionTableView.register(nib, forCellReuseIdentifier: reuseIdentifier)
     }
 
+}
+
+extension SideMenuViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SideMenuCell
+        return cell
+    }
+    
+    
 }
